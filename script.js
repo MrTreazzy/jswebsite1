22e51802f357675da4bf37214300dc4d27b8961d
MrTreazzy <lentik.vadik@gmail.com>
2022-07-30 21:37:12 +0300

    init commit

--- script.js
+++ script.js
@@ -65,28 +65,56 @@ setTimeout(()=> {
 	}
 }, 7000)
 
+
 let leth1 = document.getElementById('leth1');
 
-var letters = ['ш','щ','а','р','о','в','н','е','ё','с','з','д','и','й','ы','у','б','ц','я','к','п','л','т','м','г','ж','ч','ф','ю',' вес',' шар',' ров',' нерв',' роса',' сено',' норов',' весна',' аврора',' нос',' сон',' раз',' зуб',' бор',' рак',' код',' дар',' ряд',' дуб',' сын',' баня',' ядро',' один',' нива',' арбуз',' знак',' конец',' цапля',' время',' образ',' зрение',' год',' лев',' пуд',' том',' мор',' суп',' ток',' пар',' рот',' тук',' жар',' час',' фунт',' юла',' парта',' агроном',' муравей',' дети',' игрушка',' фанера',' сюрприз',' футбол',' тамара',' паша',' лена',' мария',' толя',' федор',' юлия',' дайте',' мел',' положите',' ручку',' принесите',' журнал']
-var random1 = Math.round(Math.random() * letters.length)
+var words = [' вес',' шар',' ров',' нерв',' роса',' сено',' норов',' весна',' аврора',' нос',' сон',' раз',' зуб',' бор',' рак',' код',' дар',' ряд',' дуб',' сын',' баня',' ядро',' один',' нива',' арбуз',' знак',' конец',' цапля',' время',' образ',' зрение',' год',' лев',' пуд',' том',' мор',' суп',' ток',' пар',' рот',' тук',' жар',' час',' фунт',' юла',' парта',' агроном',' муравей',' дети',' игрушка',' фанера',' сюрприз',' футбол',' тамара',' паша',' лена',' мария',' толя',' федор',' юлия',' дайте',' мел',' положите',' ручку',' принесите',' журнал']
+var letters = ['ш','щ','а','р','о','в','н','е','ё','с','з','д','и','й','ы','у','б','ц','я','к','п','л','т','м','г','ж','ч','ф','ю']
+var random1 = Math.round(Math.random() * words.length)
+var random2 = Math.round(Math.random() * letters.length)
+var mode = 0
 
-function randomlettfunc() {
-	leth1.textContent = ""
-	for (let i = 0; i < 8; i++) {
-		random1 = Math.round(Math.random() * letters.length)
-		if (letters[random1] != undefined) {
-			leth1.textContent += letters[random1]
+function randomwordsfunc() {
+	if (mode == 0) {
+		leth1.textContent = ""
+		for (let i = 0; i < 8; i++) {
+			random1 = Math.round(Math.random() * words.length)
+			if (words[random1] == undefined) {
+				random1 = Math.round(Math.random() * words.length)
+				leth1.textContent += words[random1]
+			} else {
+				leth1.textContent += words[random1]
+			}
+		}
+	} else {
+		leth1.textContent = ""
+		for (let i = 0; i < 10; i++) {
+			random2 = Math.round(Math.random() * letters.length)
+			if (letters[random2] != undefined) {
+				leth1.textContent += letters[random2]
+			} else {
+				random2 = Math.round(Math.random() * letters.length)
+				leth1.textContent += letters[random2]
+			}
 		}
 	}
 }
 
-leth1.onclick = () => {randomlettfunc();}
+leth1.onclick = () => {randomwordsfunc();}
 
 document.addEventListener('keydown', logKey);
-
 function logKey(e) {
 	let a = `${e.key}`
 	if (a == " ") {
-		randomlettfunc();
+		randomwordsfunc();
+	}
+	if (a == "Tab") {
+		if (mode == 0) {
+			mode = 1
+		} else {
+			mode = 0
+		}
+		randomwordsfunc();
 	}
 }
+// 'Ш','Щ','А','Р','О','В','Н','Е','Ё','С','З','Д','И','Й','Ы','У','Б','Ц','Я','К','П','Л','Т','М','Г','Ж','Ч','Ф','Ю'
